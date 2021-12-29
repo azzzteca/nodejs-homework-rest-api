4de901fd94ae1d09756915ba750b683dc91c6fb9
Andrii Shovkun <andrii.shovkun@gmail.com>
2021-12-29 21:33:18 +0200

    hw06 done

--- controllers/usersControllers/index.js
+++ controllers/usersControllers/index.js
@@ -1,9 +1,11 @@
 const getCurrent = require('./getCurrent');
 const upateSubscription = require('./upateSubscription');
 const updateAvatar = require('./updateAvatar');
+const verifyEmail = require('./verifyEmail');
 
 module.exports = {
   getCurrent,
   upateSubscription,
   updateAvatar,
+  verifyEmail,
 };
--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.patch('/', auth, ctrlWrapper(ctrl.upateSubscription));
 router.get('/current', auth, ctrlWrapper(ctrl.getCurrent));
 router.patch('/avatars', auth, upload.single('avatar'), ctrlWrapper(ctrl.updateAvatar));
+router.get('/verify/:verificationToken', ctrlWrapper(ctrl.verifyEmail));
 
 module.exports = router;
